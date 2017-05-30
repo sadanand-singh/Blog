6f745a350ac8063cb0edbce56aeade620f028f15
Sadanand Singh <sadanand4singh@gmail.com>
2017-05-30 14:50:09 -0700

    Update config file for markdown support
    including proper extensions
    
    
    Former-commit-id: 0868d9c71a429e6ff0d8ef652b70b94930bc908c [formerly f552a50fbfcd9a66c4738fd791222803d910763d]
    Former-commit-id: ea72df4d383e51a4ce0c5ce2cf6a4f46a16fd82a

--- conf.py
+++ conf.py
@@ -176,11 +176,17 @@ THEME_COLOR = '#5670d4'
 POSTS = (
     ("posts/*.rst", "posts", "post.tmpl"),
     ("posts/*.txt", "posts", "post.tmpl"),
+    ("posts/*.md", "posts", "post.tmpl"),
+    ("posts/*.mdown", "posts", "post.tmpl"),
+    ("posts/*.markdown", "posts", "post.tmpl"),
     ("posts/*.ipynb", "posts", "post.tmpl"),
 )
 PAGES = (
     ("stories/*.rst", "", "story.tmpl"),
     ("stories/*.txt", "", "story.tmpl"),
+    ("stories/*.md", "", "story.tmpl"),
+    ("stories/*.mdown", "", "story.tmpl"),
+    ("stories/*.markdown", "", "story.tmpl"),
 )
 
 
@@ -1007,7 +1013,7 @@ delimiters: [
 # Note: most Nikola-specific extensions are done via the Nikola plugin system,
 #       with the MarkdownExtension class and should not be added here.
 # The default is ['fenced_code', 'codehilite']
-MARKDOWN_EXTENSIONS = ['toc', 'fenced_code', 'codehilite', 'extra']
+MARKDOWN_EXTENSIONS = ['toc', 'fenced_code', 'codehilite', 'extra', 'admonition', 'sane_lists', 'wikilinks']
 
 # Extra options to pass to the pandoc comand.
 # by default, it's empty, is a list of strings, for example
