03abb59acedcc078a2d1a633342a58c3a741777d
Sadanand Singh <sadanand4singh@gmail.com>
2018-12-16 16:50:46 -0800

    fix publish.sh to automatically scp files

--- publish.sh
+++ publish.sh
@@ -15,3 +15,6 @@ mkdir public
 echo "Generating site"
 hugo --gc --minify
 
+# transfer
+scp -P 2222 -r public/2014 public/2015 public/404.html public/FibonacciNumbers public/PalakPaneer public/PrimeNumberAndPath public/RplotsFacebbok public/archives public/authors public/categories public/css public/favicon.ico public/index.html public/index.json public/js public/post public/posts public/project public/sitemap.xml public/tags  datascl2@162.241.224.119:~/public_html/
+
